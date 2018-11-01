cce1dee94c7b5ed3bef9ca7f51197ef66339430b
David Stosik <david.stosik+git-noreply@gmail.com>
2018-11-01 14:53:02 +0000

    Install Pebble-Clay dependency

--- pebble/src/pkjs/index.js
+++ pebble/src/pkjs/index.js
@@ -1,1 +1,11 @@
 require('pebblejs');
+var Clay = require('pebble-clay');
+
+var clayConfig = require('./config.json');
+var clay = new Clay(clayConfig);
+
+function getClaySettings() {
+  return JSON.parse(localStorage.getItem('clay-settings'));
+}
+
+var claySettings = getClaySettings();
